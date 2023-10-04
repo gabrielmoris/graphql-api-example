68ce21c83bc79ea1703469e8b7f7b1e51d2656e1
gabriel <gabrielcmoris@gmail.com>
2023-10-04 16:05:35 +0200

    add types definition and start with resolvers

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -2,6 +2,7 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type User {
+    id: ID!
     name: String!
     username: String!
     age: Int!
@@ -12,3 +13,5 @@ const typeDefs = gql`
     users: [User!]!
   }
 `;
+
+module.exports = { typeDefs };
